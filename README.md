6a181cb0d5cca69b7a77176408c48a6865db11d9
JounQin <liujiaren1992@vip.qq.com>
2017-05-17 17:24:50 +0800

    first blood, basic structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # touch-lite
-A lite touch library without any dependecy.
+A lite touch library without any dependency.
+
+*This module is still working in progress right now, so it is just for placeholder.*
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
+
+const pkg = require('./package.json')
+
+const DEV = 'development'
+
+const NODE_ENV = process.env.NODE_ENV || DEV
+
+const isDev = NODE_ENV === DEV
+
+const plugins = [buble()]
+
+isDev || plugins.push(uglify({
+  output: {
+    comments: true
+  }
+}))
+
+export default {
+  banner: `/*!
+ * ${pkg.name}: ${pkg.description}
+ * Version ${pkg.version}
+ * Copyright (C) 2017 JounQin <admin@1stg.me>
+ * Released under the ${pkg.license} license
+ *
+ * Github: https://github.com/JounQin/touch-lite
+ */`,
+  entry: 'lib/index.js',
+  dest: `dist/touch${isDev ? '' : '.min'}.js`,
+  format: 'umd',
+  moduleName: 'Touch',
+  moduleId: pkg.name,
+  plugins
+}
