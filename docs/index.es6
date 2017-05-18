3d978c787d37a2de05a3f1673e092191cd142fcd
JounQin <liujiaren1992@vip.qq.com>
2017-05-18 10:29:32 +0800

    1. fix issues 2. improve compatibility with IE 9- 3. add demo page

--- docs/index.es6
+++ docs/index.es6
@@ -1,6 +1,32 @@
 const demoEl = document.getElementById('demo')
 
-const touch = new Touch(demoEl).on('moveStart', function (e) {
-  console.log(this)
-  console.log(e.changedX, e.changedY, e._clientX, e._clientY)
-})
+const currPosition = () => {
+  let left
+  let top
+
+  const style = demoEl.currentStyle ? demoEl.currentStyle : getComputedStyle(demoEl)
+  left = style.left
+  top = style.top
+
+  left = left === 'auto' ? 0 : +left.replace('px', '')
+  top = top === 'auto' ? 0 : +top.replace('px', '')
+
+  return {
+    left,
+    top
+  }
+}
+
+let leftStart, topStart
+
+const touch = new Touch(demoEl)
+  .on('moveStart', function () {
+    const position = currPosition()
+
+    leftStart = position.left
+    topStart = position.top
+  })
+  .on('moving', function () {
+    demoEl.style.left = leftStart + this.changedX + 'px'
+    demoEl.style.top = topStart + this.changedY + 'px'
+  })
--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,33 @@
 
 var demoEl = document.getElementById('demo');
 
-var touch = new Touch(demoEl).on('moveStart', function (e) {
-  console.log(this);
-  console.log(e.changedX, e.changedY, e._clientX, e._clientY);
+var currPosition = function currPosition() {
+  var left = void 0;
+  var top = void 0;
+
+  var style = demoEl.currentStyle ? demoEl.currentStyle : getComputedStyle(demoEl);
+  left = style.left;
+  top = style.top;
+
+  left = left === 'auto' ? 0 : +left.replace('px', '');
+  top = top === 'auto' ? 0 : +top.replace('px', '');
+
+  return {
+    left: left,
+    top: top
+  };
+};
+
+var leftStart = void 0,
+    topStart = void 0;
+
+var touch = new Touch(demoEl).on('moveStart', function () {
+  var position = currPosition();
+
+  leftStart = position.left;
+  topStart = position.top;
+}).on('moving', function () {
+  demoEl.style.left = leftStart + this.changedX + 'px';
+  demoEl.style.top = topStart + this.changedY + 'px';
 });
 //# sourceMappingURL=index.js.map
